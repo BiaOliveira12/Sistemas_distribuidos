a9ad8110a5d7b803aa63788b83b9b57b0529a11c
BiaOliveira12 <aguiarbeatriz12ba@gmail.com>
2025-04-10 10:45:46 -0300

    commit

--- Aula06-REST/API/server.js
+++ Aula06-REST/API/server.js
@@ -1,5 +1,7 @@
+const { setFips } = require('crypto');
 var express = require('express');
 var http = require('http');
+const { connect } = require('http2');
 var mysql = require('mysql');
 
 var conn = mysql.createConnection( {
@@ -9,12 +11,35 @@ var conn = mysql.createConnection( {
     database : "loja"
 } );
 
+try{
+    if(conn.state != "authenticated"){
+        conn.connect( function(erro){
+            if(erro){
+                console.log(erro);
+            }
+        } );
+    }
+}catch(error){
+    console.log(erro);
+}
 
 var app = express();
 
 app.get('/', (req, res) => { 
     res.status( 200 ).send( "Bem-vindo(a) à nossa API REST" );
+});
 
+app.get('/produto', (req, res) => { 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    var sql = "SELECT * FROM produto ORDER BY nome";
+    conn.query(sql, function(err, result, fields){
+        if(err){
+            res.send('{ "resposta": "Erro ao executar a consulta" }');
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    }); 
 });
 
 http.createServer(app).listen(8001, () => {
